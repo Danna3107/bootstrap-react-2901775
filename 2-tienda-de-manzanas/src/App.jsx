ae7f0feb6d912c3c34bba5a3f81bb2e5b7dc9fc1
Danna3107 <169182798+Danna3107@users.noreply.github.com>
2024-10-25 07:20:07 -0500

    creacion card bootstrap

---
+++ 2-tienda-de-manzanas/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Section from "./components/Section"
+import Article from "./components/Article"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function App() {
+  
+
+  return (
+   <div>
+    <Header />
+    <Section/>
+    <Article />
+    <Footer/>
+   </div>
+  )
+}
+
+export default App
