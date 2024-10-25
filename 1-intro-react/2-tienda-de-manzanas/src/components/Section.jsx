9acf337eb98a6637d8238f88910a4dead1a4be12
Danna3107 <169182798+Danna3107@users.noreply.github.com>
2024-10-25 07:21:43 -0500

    card

---
+++ 1-intro-react/2-tienda-de-manzanas/src/components/Section.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Section() {
+  return (
+<>
+<section>
+  <div className="container">
+    <h1 className='text warning'>seccion de promos Naranjas🍊</h1>
+    <div className="row">
+    <div className="col">
+    <div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGr4hFOgZEBL5T_D8Z8hgy87yfUi5PBqKfkw&s" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+
+  </div>
+</div>
+    </div>
+    </div>
+  </div>
+</section>
+</>
+  )
+}
